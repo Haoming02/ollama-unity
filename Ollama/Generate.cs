2153b3d0581a5481b79fe206e1a95ca6f5d721e0
Haoming <hmstudy02@gmail.com>
2025-03-26 18:16:51 +0800

    generate

--- Ollama/Generate.cs
+++ Ollama/Generate.cs
@@ -7,52 +7,44 @@ namespace ollama
 {
     public static partial class Ollama
     {
-        /// <summary>
-        /// Generate a simple response from prompt <i>(no context/history)</i>
-        /// </summary>
-        /// <param name="model">Ollama Model Syntax (<b>eg.</b> llama3.1)</param>
-        /// <param name="images">A multimodal model is required to handle images (<b>eg.</b> llava)</param>
-        /// <param name="keep_alive">The behavior to keep the model loaded in memory</param>
+        /// <summary>Generate a simple response from prompt</summary>
+        /// <param name="model">Ollama Model Syntax (<b>eg.</b> gemma3:4b)</param>
+        /// <param name="keep_alive">The duration <i>(in seconds)</i> to keep the model in memory</param>
         /// <returns>response string from the LLM</returns>
-        public static async Task<string> Generate(string model, string prompt, Texture2D[] images = null, KeepAlive keep_alive = KeepAlive.five_minute)
+        public static async Task<string> Generate(string model, string prompt, int keep_alive = 300, Texture2D[] images = null)
         {
             var imagesBase64 = EncodeTextures(images);
-            var request = new Request.Generate(model, prompt, imagesBase64, null, false, keep_alive);
+            var request = new Request.Generate(model, prompt, false, keep_alive, null, imagesBase64);
             string payload = JsonConvert.SerializeObject(request);
             var response = await PostRequest<Response.Generate>(payload, Endpoints.GENERATE);
             return response.response;
         }
 
-        /// <summary>
-        /// Generate a simple response in JSON format from prompt
-        /// </summary>
-        /// <typeparam name="T">The structure of the JSON object</typeparam>
-        /// <param name="model">Ollama Model Syntax (<b>eg.</b> llama3.1)</param>
-        /// <param name="prompt">You need to manaully tell the LLM to return in a JSON format</param>
-        /// <param name="images">A multimodal model is required to handle images (<b>eg.</b> llava)</param>
-        /// <param name="keep_alive">The behavior to keep the model loaded in memory</param>
-        /// <returns>response string from the LLM</returns>
-        public static async Task<T> GenerateJson<T>(string model, string prompt, Texture2D[] images = null, KeepAlive keep_alive = KeepAlive.five_minute)
+        /// <summary>Generate a simple response in JSON format from prompt</summary>
+        /// <typeparam name="T">The class/struct of the JSON object</typeparam>
+        /// <param name="model">Ollama Model Syntax (<b>eg.</b> gemma3:4b)</param>
+        /// <param name="prompt"><b>Important:</b> You need to manaully tell the LLM to reply in the JSON format</param>
+        /// <param name="keep_alive">The duration <i>(in seconds)</i> to keep the model in memory</param>
+        /// <returns>response object from the LLM</returns>
+        public static async Task<T> GenerateJson<T>(string model, string prompt, int keep_alive = 300, Texture2D[] images = null)
         {
             var imagesBase64 = EncodeTextures(images);
-            var request = new Request.Generate(model, prompt, imagesBase64, "json", false, keep_alive);
+            var request = new Request.Generate(model, prompt, false, keep_alive, "json", imagesBase64);
             string payload = JsonConvert.SerializeObject(request);
             var response = await PostRequest<Response.Generate>(payload, Endpoints.GENERATE);
             return JsonConvert.DeserializeObject<T>(response.response);
         }
 
-        /// <summary>
-        /// Stream a simple response from prompt <i>(no context/history)</i>
-        /// </summary>
+        /// <summary>Stream a simple response from prompt</summary>
         /// <param name="onTextReceived">The callback to handle the streaming chunks</param>
-        /// <param name="model">Ollama Model Syntax (<b>eg.</b> llama3.1)</param>
-        /// <param name="images">A multimodal model is required to handle images (<b>eg.</b> llava)</param>
-        /// <param name="keep_alive">The behavior to keep the model loaded in memory</param>
-        public static async Task GenerateStream(Action<string> onTextReceived, string model, string prompt, Texture2D[] images = null, KeepAlive keep_alive = KeepAlive.five_minute)
+        /// <param name="model">Ollama Model Syntax (<b>eg.</b> gemma3:4b)</param>
+        /// <param name="keep_alive">The duration <i>(in seconds)</i> to keep the model in memory</param>
+        public static async Task GenerateStream(Action<string> onTextReceived, string model, string prompt, int keep_alive = 300, Texture2D[] images = null)
         {
             var imagesBase64 = EncodeTextures(images);
-            var request = new Request.Generate(model, prompt, imagesBase64, null, true, keep_alive);
+            var request = new Request.Generate(model, prompt, true, keep_alive, null, imagesBase64);
             string payload = JsonConvert.SerializeObject(request);
+
             await PostRequestStream(payload, Endpoints.GENERATE, (Response.Generate response) =>
             {
                 if (!response.done)
