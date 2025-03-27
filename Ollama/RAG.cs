c316c187cbe89e7ca502d86d1338848104f15cdf
Haoming <hmstudy02@gmail.com>
2025-03-27 11:05:39 +0800

    rag

--- Ollama/RAG.cs
+++ Ollama/RAG.cs
@@ -2,6 +2,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -9,7 +10,13 @@ namespace ollama
 {
     public static partial class Ollama
     {
-        private const int CONTEXT_LIMIT = 1024;
+        /// <summary>
+        /// 1 <b>Token</b> is roughly 1 English <b>word</b> or 1 Chinese <b>character</b> <br/>
+        /// * <see href="https://ollama.com/library/nomic-embed-text">nomic-embed-text</see> has a context window of <b>8192</b> tokens<br/>
+        /// * <see href="https://ollama.com/library/gemma3">gemma3</see> has a context window of <b>128k</b> tokens<br/>
+        /// So, a limit of 8192 should be fine nowadays <i>(if your document is longer than this, better to split it yourself...)</i>
+        /// </summary>
+        private const int CONTEXT_LIMIT = 8192;
 
         private static List<float[]> VectorsDatabase;
         private static List<string> ContextDatabase;
@@ -17,31 +24,23 @@ namespace ollama
         private static class EmbedConfig
         {
             public static string model;
-            public static KeepAlive keep_alive;
+            public static int keep_alive;
         }
 
-        /// <summary>
-        /// Initialize the database to store the vectors
-        /// </summary>
-        /// <param name="embeddingModel">
-        /// Ollama Model Syntax (<b>eg.</b> llama3.1) <br/>
-        /// <b>Note:</b> Not all models can generate Embeddings
-        /// </param>
-        /// <param name="keep_alive">
-        /// The behavior to keep the embedding model loaded in memory
-        /// </param>
-        public static void InitRAG(string embeddingModel, KeepAlive keep_alive = KeepAlive.five_minute)
+        /// <summary>Initialize the database to store the vectors</summary>
+        /// <param name="model">Ollama Model Syntax (<b>eg.</b> nomic-embed-text)</param>
+        /// <param name="keep_alive">The duration <i>(in seconds)</i> to keep the model in memory</param>
+        public static void InitRAG(string model, int keep_alive = 300)
         {
             VectorsDatabase = new List<float[]>();
             ContextDatabase = new List<string>();
-            EmbedConfig.model = embeddingModel;
+
+            EmbedConfig.model = model;
             EmbedConfig.keep_alive = keep_alive;
         }
 
-        /// <summary>
-        /// Add context into the database and calculate vector for it
-        /// </summary>
-        /// <param name="data">Ensure longer texts contains newline in order to be split</param>
+        /// <summary>Add a context into the database and calculate its vector</summary>
+        /// <param name="data"><b>Note:</b> Ensure longer texts contain newline character, in order to be split</param>
         public static async Task AppendData(string data)
         {
             int len = data.Length;
@@ -53,34 +52,40 @@ namespace ollama
                 return;
             }
 
-            int chunkCount = Mathf.CeilToInt((float)len / CONTEXT_LIMIT);
-            int contextLength = len / chunkCount;
-
-            string[] lines = data.Split('\n', StringSplitOptions.RemoveEmptyEntries);
-            int lineCount = lines.Length;
-            int i = 0;
-
+            string[] sentences = Regex.Split(data, @"(?<=[.?!""])\s+");
             StringBuilder chunk = new StringBuilder();
 
-            while (i < lineCount)
+            foreach (string sentence in sentences)
             {
-                chunk.Clear();
-
-                while (chunk.Length < contextLength && i < lineCount)
+                if (sentence.Length > CONTEXT_LIMIT)
                 {
-                    string line = lines[i].Trim();
-                    if (line.Length > contextLength && chunk.Length > contextLength / 2)
-                        break;
-
-                    chunk.AppendLine(line);
-                    i++;
+                    string paragraph = chunk.ToString();
+                    float[] vector = await Embeddings(EmbedConfig.model, paragraph, EmbedConfig.keep_alive);
+                    VectorsDatabase.Add(vector);
+                    ContextDatabase.Add(paragraph);
+                    chunk.Clear();
                 }
 
-                string paragraph = chunk.ToString();
+                chunk.Append(" ");
+                chunk.Append(sentence);
 
+                if (chunk.Length > CONTEXT_LIMIT)
+                {
+                    string paragraph = chunk.ToString();
+                    float[] vector = await Embeddings(EmbedConfig.model, paragraph, EmbedConfig.keep_alive);
+                    VectorsDatabase.Add(vector);
+                    ContextDatabase.Add(paragraph);
+                    chunk.Clear();
+                }
+            }
+
+            if (chunk.Length > 0)
+            {
+                string paragraph = chunk.ToString();
                 float[] vector = await Embeddings(EmbedConfig.model, paragraph, EmbedConfig.keep_alive);
                 VectorsDatabase.Add(vector);
                 ContextDatabase.Add(paragraph);
+                chunk.Clear();
             }
         }
 
@@ -91,7 +96,8 @@ namespace ollama
 
             if (l == 1)
                 return Context2Message(ContextDatabase[0]);
-            else if (l == 0)
+
+            if (l == 0)
             {
                 Debug.LogError("Database is Empty...");
                 return null;
@@ -116,31 +122,33 @@ namespace ollama
             return Context2Message(ContextDatabase[index]);
         }
 
-        /// <summary>
-        /// Ask a question based on the given context
-        /// </summary>
-        /// <param name="model">Ollama Model Syntax (<b>eg.</b> llama3.1)</param>
-        /// <param name="keep_alive">The behavior to keep the model loaded in memory</param>
+        /// <summary>Ask a question based on the given context</summary>
+        /// <param name="model">Ollama Model Syntax (<b>eg.</b> gemma3:4b)</param>
+        /// <param name="keep_alive">The duration <i>(in seconds)</i> to keep the model in memory</param>
         /// <returns>response string from the LLM</returns>
-        public static async Task<string> Ask(string model, string prompt, KeepAlive keep_alive = KeepAlive.five_minute)
+        public static async Task<string> Ask(string model, string prompt, int keep_alive = 300)
         {
             var system = await Analyze(prompt);
-            var request = new Request.Chat(model, new Message[] { system, new Message("user", $"Question:\n{prompt}") }, false, keep_alive);
+            var query = new Message("user", $"Question:\n{prompt}");
+            var messages = new List<Message>() { system, query };
+
+            var request = new Request.Chat(model, messages, false, keep_alive);
             string payload = JsonConvert.SerializeObject(request);
             var response = await PostRequest<Response.Chat>(payload, Endpoints.CHAT);
             return response.message.content;
         }
 
-        /// <summary>
-        /// Stream an answer based on the given context
-        /// </summary>
+        /// <summary>Stream an answer based on the given context</summary>
         /// <param name="onTextReceived">The callback to handle the streaming chunks</param>
-        /// <param name="model">Ollama Model Syntax (<b>eg.</b> llama3.1)</param>
-        /// <param name="keep_alive">The behavior to keep the model loaded in memory</param>
-        public static async Task AskStream(Action<string> onTextReceived, string model, string prompt, KeepAlive keep_alive = KeepAlive.five_minute)
+        /// <param name="model">Ollama Model Syntax (<b>eg.</b> gemma3:4b)</param>
+        /// <param name="keep_alive">The duration <i>(in seconds)</i> to keep the model in memory</param>
+        public static async Task AskStream(Action<string> onTextReceived, string model, string prompt, int keep_alive = 300)
         {
             var system = await Analyze(prompt);
-            var request = new Request.Chat(model, new Message[] { system, new Message("user", $"Question:\n{prompt}") }, true, keep_alive);
+            var query = new Message("user", $"Question:\n{prompt}");
+            var messages = new List<Message>() { system, query };
+
+            var request = new Request.Chat(model, messages, true, keep_alive);
             string payload = JsonConvert.SerializeObject(request);
 
             await PostRequestStream(payload, Endpoints.CHAT, (Response.Chat response) =>
@@ -155,10 +163,9 @@ namespace ollama
         private static Message Context2Message(string context)
         {
             return new Message("system",
-                "You are an assistant for question-answering tasks. " +
-                "Use only the following piece of context to answer the question. " +
-                "If no answer can be found within the context, simply say so. Do not answer outside of context. \n" +
-                $"Context:\n```\n{context}\n```"
+                "You are an assistant for question-answering tasks. Use only the following piece of context to answer the question. " +
+                "If no answer can be found within the context, simply say so. Do **NOT** answer outside of context. \n" +
+                $"Context:\n\"\"\"\n{context}\n\"\"\""
             );
         }
 
